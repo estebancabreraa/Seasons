231c24f098c29706e45dfa99b3b72dc28529ab0f
estebancabreraa <esteban.cabrera.a@gmail.com>
2018-02-27 23:07:48 -0600

    Botones en escenas

---
+++ Seasons/Assets/Scripts/Pausa.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pausa : MonoBehaviour {
+    public Button yourButton;
+	// Use this for initialization
+	void Start () {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+    // Update is called once per frame
+    
+}
